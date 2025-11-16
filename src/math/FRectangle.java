bb157d43de84782d459a6acfb5840e50d100cb2e
Fipaan <fipaan@proton.me>
2025-11-16 23:03:40 +0500

    Fixed constructors

--- src/math/FRectangle.java
+++ src/math/FRectangle.java
@@ -11,24 +11,24 @@ public class FRectangle implements HasRect<FRectangle> {
         this.width  = w;
         this.height = h;
     }
-    public FRectangle(double x, double y, double s)             { this(x,        y,        s,               s);                }
-    public FRectangle(double x, double y, Dimension s)          { this(x,        y,        s.width,         s.height);         }
-    public FRectangle(double x, double y, HasSize<?> size)      { this(x,        y,        s.getWidth(),    s.getHeight());    }
-    public FRectangle(double w, double h)                       { this(0,        0,        w,               h);                }
-    public FRectangle(double s)                                 { this(0,        0,        s,               s);                }
-    public FRectangle(Dimension s)                              { this(0,        0,        s.width,         s.height);         }
-    public FRectangle(HasSize<?>    size)                       { this(0,        0,        size.getWidth(), size.getHeight()); }
-    public FRectangle(Point p, double w, double h)              { this(p.x,      p.y,      w,               h);                }
-    public FRectangle(Point p, double s)                        { this(p.x,      p.y,      s,               s);                }
-    public FRectangle(Point p, Dimension s)                     { this(p.x,      p.y,      s.width,         s.height);         }
-    public FRectangle(Point p, HasSize<?> size)                 { this(p.x,      p.y,      s.getWidth(),    s.getHeight());    }
-    public FRectangle(Point p)                                  { this(p.x,      p.y,      0,               0);                }
-    public FRectangle(HasLocation<?> loc, double w, double h)   { this(p.getX(), p.getY(), w,               h);                }
-    public FRectangle(HasLocation<?> loc, double s)             { this(p.getX(), p.getY(), s,               s);                }
-    public FRectangle(HasLocation<?> loc, Dimension s)          { this(p.getX(), p.getY(), s.width,         s.height);         }
-    public FRectangle(HasLocation<?> loc, HasSize<?> size)      { this(p.getX(), p.getY(), s.getWidth(),    s.getHeight());    }
-    public FRectangle(HasLocation<?> loc)                       { this(p.getX(), p.getY(), 0,               0);                }
-    public FRectangle()                                         { this(0,        0,        0,               0);                }
+    public FRectangle(double x, double y, double s)         { this(x,        y,        s,               s);                }
+    public FRectangle(double x, double y, Dimension s)      { this(x,        y,        s.width,         s.height);         }
+    public FRectangle(double x, double y, HasSize<?> s)     { this(x,        y,        s.getWidth(),    s.getHeight());    }
+    public FRectangle(double w, double h)                   { this(0,        0,        w,               h);                }
+    public FRectangle(double s)                             { this(0,        0,        s,               s);                }
+    public FRectangle(Dimension s)                          { this(0,        0,        s.width,         s.height);         }
+    public FRectangle(HasSize<?> s)                         { this(0,        0,        s.getWidth(),    s.getHeight());    }
+    public FRectangle(Point p, double w, double h)          { this(p.x,      p.y,      w,               h);                }
+    public FRectangle(Point p, double s)                    { this(p.x,      p.y,      s,               s);                }
+    public FRectangle(Point p, Dimension s)                 { this(p.x,      p.y,      s.width,         s.height);         }
+    public FRectangle(Point p, HasSize<?> s)                { this(p.x,      p.y,      s.getWidth(),    s.getHeight());    }
+    public FRectangle(Point p)                              { this(p.x,      p.y,      0,               0);                }
+    public FRectangle(HasLocation<?> p, double w, double h) { this(p.getX(), p.getY(), w,               h);                }
+    public FRectangle(HasLocation<?> p, double s)           { this(p.getX(), p.getY(), s,               s);                }
+    public FRectangle(HasLocation<?> p, Dimension s)        { this(p.getX(), p.getY(), s.width,         s.height);         }
+    public FRectangle(HasLocation<?> p, HasSize<?> s)       { this(p.getX(), p.getY(), s.getWidth(),    s.getHeight());    }
+    public FRectangle(HasLocation<?> p)                     { this(p.getX(), p.getY(), 0,               0);                }
+    public FRectangle()                                     { this(0,        0,        0,               0);                }
 
     @Override public double getX()      { return x;      }
     @Override public double getY()      { return y;      }
